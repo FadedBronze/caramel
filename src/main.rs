411bcaa22134a25a8be7ef0ada6d266bf755121e
FadedBronze <TheSamyatGautam@gmail.com>
2024-11-06 13:05:14 -0500

    adding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, env, fs};
+use std::{collections::VecDeque, env, fs, ops::Deref};
 
 #[derive(Debug, Clone)]
 struct Variable(String);
@@ -15,6 +15,7 @@ enum Token {
     Exit,
     IntLit(IntLiteral),
     Semicolon,
+    Plus,
 }
 
 impl Token {}
@@ -63,6 +64,7 @@ impl Tokenizer {
                 ')' => {tokens.push_back(Token::RightParen)},
                 ';' => {tokens.push_back(Token::Semicolon)},
                 '=' => {tokens.push_back(Token::EqualSign)},
+                '+' => {tokens.push_back(Token::Plus)},
                 _ => {
                     start -= 1;
                 }
@@ -74,15 +76,35 @@ impl Tokenizer {
 }
 
 #[derive(Debug)]
+enum Operation {
+    Plus
+}
+
+#[derive(Debug)]
 enum Value {
     Var(Variable),
     IntLit(IntLiteral)
 }
 
 #[derive(Debug)]
+enum Expr {
+    Expr(Value, Operation, Box<Expr>),
+    Value(Value)
+}
+
+impl Expr {
+    fn get_value(&self) -> Option<&Value> {
+        match self {
+            Expr::Value(value) => Some(value),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
 enum Stmt {
-    ExitStmt(Value),
-    DeclarationStmt(Variable, Value),
+    ExitStmt(Expr),
+    DeclarationStmt(Variable, Expr),
 }
 
 #[derive(Debug)]
@@ -127,6 +149,30 @@ impl Parser {
         }
     }
 
+    fn parse_op(tokens: &mut VecDeque<Token>) -> Option<Operation> {
+        match tokens[0] {
+            Token::Plus => {
+                tokens.pop_front();
+                Some(Operation::Plus)
+            },
+            _ => {
+                None
+            }
+        }
+    }
+    
+    fn parse_expr(tokens: &mut VecDeque<Token>) -> Option<Expr> {
+        let Some(a) = Parser::parse_value(tokens) else { 
+            return None; 
+        };
+
+        let Some(op) = Parser::parse_op(tokens) else { return Some(Expr::Value(a)); };
+
+        let Some(b) = Parser::parse_expr(tokens) else { return None; };
+
+        return Some(Expr::Expr(a, op, Box::new(b)));
+    }
+
     fn parse_stmt(tokens: &mut VecDeque<Token>) -> Option<Stmt> {
         match tokens[0] {
             Token::Exit => {
@@ -138,7 +184,7 @@ impl Parser {
                     return None;
                 };
 
-                let result = Stmt::ExitStmt(Parser::parse_value(tokens)?);
+                let result = Stmt::ExitStmt(Parser::parse_expr(tokens)?);
                 
                 let next = tokens.pop_front();
                 let Some(Token::RightParen) = next else { 
@@ -169,7 +215,7 @@ impl Parser {
                     return None;
                 };
                 
-                let result = Stmt::DeclarationStmt(dest, Parser::parse_value(tokens)?);
+                let result = Stmt::DeclarationStmt(dest, Parser::parse_expr(tokens)?);
 
                 let next = tokens.pop_front();
                 let Some(Token::Semicolon) = next else { 
@@ -222,7 +268,7 @@ impl Codegen {
         self.output_string += format!("    pop {}\n", reg).as_str();
         self.stack_size -= 1;
     }
-
+    
     fn gen_value(&mut self, value: &Value) {
         match value {
             Value::IntLit(IntLiteral(num)) => {
@@ -231,7 +277,35 @@ impl Codegen {
             }
             Value::Var(Variable(varname)) => {
                 let var_position = self.varnames.iter().find(|var| var.0 == *varname).take().unwrap().1;
-                self.output_string += format!("    mov rax, [rsp + 8 * {}]\n", var_position).as_str();
+                self.output_string += format!("    mov rax, [rsp + 8 * {}] ; {}\n",  self.stack_size - var_position, varname).as_str();
+                self.push("rax");
+            }
+        }
+    }
+    
+    fn gen_expr(&mut self, value: &Expr) {
+        match &value {
+            Expr::Value(value) => {
+                self.gen_value(value)
+            },
+            Expr::Expr(a, op, expr) => {
+                self.gen_value(a);
+
+                if let Some(b) = expr.get_value() {
+                    self.gen_value(b);
+                } else {
+                    self.gen_expr(expr);    
+                }
+
+                self.pop("rax");
+                self.pop("r8");
+
+                match op {
+                    Operation::Plus => {
+                        self.output_string += format!("    add rax, r8\n").as_str();
+                    }
+                }
+                
                 self.push("rax");
             }
         }
@@ -239,15 +313,12 @@ impl Codegen {
 
     fn generate_stmt(&mut self, statement: &Stmt) {
         match &statement {
-            Stmt::DeclarationStmt(Variable(varname), value) => {
-                self.gen_value(value);
-                self.pop("rdi");
-
+            Stmt::DeclarationStmt(Variable(varname), expr) => {
+                self.gen_expr(expr);
                 self.varnames.push((varname.to_string(), self.stack_size));
-                self.push("rax");
             }
             Stmt::ExitStmt(value) => {
-                self.gen_value(value);
+                self.gen_expr(value);
                 self.output_string += format!("    mov rax, 60\n").as_str();
                 self.pop("rdi");
                 self.output_string += format!("    syscall\n").as_str();
@@ -261,6 +332,7 @@ impl Codegen {
 
         for statement in ast.statements.iter() {
             self.generate_stmt(statement);
+            self.output_string += format!("\n").as_str();
         }
 
         let result = self.output_string.clone();
@@ -285,8 +357,9 @@ fn main() {
     };
 
     let tokens = Tokenizer::tokenize(&bytes);
+    println!("{:?}", tokens);
     let ast = Parser::parse(tokens).unwrap();
+    println!("{:#?}", ast);
     let program = Codegen::new().generate(&ast);
     println!("{}", program);
-    //let result = Codegen::generate(&tokens);
 }
